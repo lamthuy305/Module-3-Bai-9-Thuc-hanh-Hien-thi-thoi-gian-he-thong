378c399bb06fd7c92771f429e19555a8ef3cfb94
lamthuy305 <lamthuy305@gmail.com>
2022-03-02 09:48:29 +0700

    first commit

---
+++ src/main/java/com/codegym/ServerTimeServlet.java
@@ -0,0 +1,28 @@
+package com.codegym;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", value = "/index")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        Date date = new Date();
+        writer.println("<html>");
+        writer.println("<h1>Hello world</h1>");
+        writer.println("<h1>"+date+"</h1>");
+        writer.println("</html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
